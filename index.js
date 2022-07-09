085a16697d18cff77fe582b530e2595546f5a3bb
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-08 22:51:56 -0400

    step 1

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-
+function newTask(title, description) {
+  const task = {
+    title, description, complete:false
+  };
+  return task;
+}
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
 function newTask(title) {
   taskTitles.push(title);
   taskComplete.push(false);
 }
-
+const task1 = newTask("Clean Cat litter", "Take all the shit out of the litter box");
+const task2 = newTask("Do Laundry", "😑");
+const task = [task1, task2];
+console.log(tasks);
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 function completeTask(taskIndex) {
   taskComplete[taskIndex] = true;
